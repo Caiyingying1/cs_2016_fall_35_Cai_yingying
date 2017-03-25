8cb7bf19d69d555b7a6bdb1c256fd6d4560a47dd
Caiyingying1 <809874260@qq.com>
2017-03-26 00:58:42 +0300

    Add files via upload

---
+++ язык с/C 13.cpp
@@ -0,0 +1,37 @@
+# include <stdio.h>
+# define MAX 20
+# define OUT 0
+# define IN 1
+
+int main(void)
+{
+	int length[MAX];
+	int c;
+	int vocl = 0;
+	int state = OUT;
+	int i;
+	
+	for(vocl = 0; vocl < MAX; vocl++){
+		length[vocl] = 0;
+	}
+	vocl = 0; 
+	while((c = getchar()) != EOF){
+		if(c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c == '-'){
+			vocl++;
+		} else{
+			length[vocl]++;
+			vocl = 0;
+		}
+	}
+
+	for(vocl = 0; vocl < MAX; vocl++){
+		if(length[vocl] != 0){
+			printf("%2d  ", vocl);
+			for(i = 1; i <= length[vocl]; i++){
+				putchar('*');
+			}
+			putchar('\n');
+		}
+	}
+}
+	
