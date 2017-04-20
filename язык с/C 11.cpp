f93707c945ef82fd0e959c3abf41ae9673bf62b5
Caiyingying1 <809874260@qq.com>
2017-04-20 10:27:18 +0300

    Update C 11.cpp

--- язык с/C 11.cpp
+++ язык с/C 11.cpp
@@ -1,19 +1,16 @@
 #include<stdio.h>
-#include<stdlib.h>
-int  main()
+int main()
 {
-   int i,n,k=1,a[10]; 
-   printf("enter n:");
-   scanf("%d",&n);
-   if(n<=0) exit(0); 
-   a[0]=n%2;
-   do
-  {
-    n=n/2;
-    i=n%2;
-    a[k++]=i;
-   }while(n!=0);
-   for(i=9;a[i]==0;i--) ;  
-     for(;i>-1;i--)
-          printf("%d",a[i]);
+int num = 0, a, j=1;
+scanf("%d", &a);
+
+while(a)
+{
+num += (a%10) * j;
+a /= 10;
+j *= 2;
+}
+printf("%d\n", num);
+return 0;
+
 }
