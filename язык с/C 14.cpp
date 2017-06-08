9a32dca8b766b782e94d573b95dfe2e2c7ce9a58
Caiyingying1 <809874260@qq.com>
2017-06-08 20:31:23 +0300

    Create C 14.cpp

--- язык с/C 14.cpp
+++ язык с/C 14.cpp
@@ -1,28 +1,67 @@
-#include <stdio.h>
+#include<stdio.h>  
+       
+void Swap(char *a,char *b)  
+{  
+    char temp = *a;  
+    *a = *b;  
+    *b = temp;  
+}  
+       
 
-#include <string.h>
+void Reverse(char *str,char *begin,char *end)  
+{  
+    if(str==NULL || begin==NULL || end==NULL)  
+        return;  
+       
+    while(begin < end)  
+    {  
+        Swap(begin,end);  
+        begin++;  
+        end--;  
+    }  
+}  
+       
 
-int main()
-
-{
-
-  char str[100];
-
-  int n, i;
-
-  printf("Input a string:");
-
-  scanf("%s", str);
-
-  n = strlen(str);
-
-  printf("The reversed string:");
-
-  for (i = n - 1; i >= 0; i--)
-
-    printf("%c", str[i]);
-
-  printf("\n");
-
+void ReverseSentence(char *str)  
+{  
+    if(str == NULL)  
+        return;  
+       
+    char *begin = str;  
+    char *end = str;  
+    while(*end != '\0')  
+        end++;  
+    end--;  
+      
+    Reverse(str,begin,end);  
+       
+    
+    begin = str;  
+    while(*begin == ' ')  
+        begin++;  
+    end = begin;  
+    while(*begin != '\0')  
+    {  
+        while(*end!=' ' && *end!='\0')  
+            end++;  
+        end--;  
+        Reverse(str,begin,end);  
+       
+         
+        end++;  
+        while(*end == ' ')  
+            end++;  
+        begin = end;  
+    }  
+}  
+       
+int main()  
+{  
+    char str[50000];  
+    while(gets(str) != NULL)  
+    {  
+        ReverseSentence(str);  
+        puts(str);  
+    }  
+    return 0;  
 }
-
