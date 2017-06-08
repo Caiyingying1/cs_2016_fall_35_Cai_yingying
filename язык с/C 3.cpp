3537c86380eefd7c044b539f8d1dbe28140ea777
Caiyingying1 <809874260@qq.com>
2017-06-08 18:43:59 +0300

    Update C 3.cpp

--- язык с/C 3.cpp
+++ язык с/C 3.cpp
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
-int main()
-{
-    float r1,r2,r;
-    printf("Enter the two resistance values that need to be connected in parallel£º");
-    scanf("%f %f",&r1,&r2);
-    r=1/(1/r1+1/r2);
-    printf("The resistance value after parallel is %f\n",r);
-    return 0;
+
+
+int main(void) {
+
+	float x, y, a, b, c, d ;
+
+
+	printf("Enter numbers in the format a+bi,x+yi\n ");
+
+	scanf("%f+%fi,%f+%fi", &x, &y, &a, &b);
+
+	c = (x*x*a+x*a*a+x*b*b+a*y*y)/((x+a)*(x+a)+(y+b)*(y+b));
+
+	d = (x*x*b+a*a*y+y*y*b+y*b*b)/((x+a)*(x+a)+(y+b)*(y+b));
+
+	printf("%.3f+%.3fi", c, d);
+
+	return 0;
+
 }
