ea0003bae9e4e04e4fc02f1b72ceeeb78bfca08f
Caiyingying1 <809874260@qq.com>
2017-06-08 18:12:25 +0300

    Create C 13.cpp

--- язык с/C 13.cpp
+++ язык с/C 13.cpp
@@ -1,37 +1,137 @@
-# include <stdio.h>
-# define MAX 20
-# define OUT 0
-# define IN 1
+#include <stdio.h>
 
-int main(void)
+
+
+#define MAXHIST 20  
+
+#define MAXLEN 10   
+
+#define IN 1
+
+#define OUT 0
+
+
+
+void main()
+
 {
-	int length[MAX];
-	int c;
-	int vocl = 0;
-	int state = OUT;
-	int i;
-	
-	for(vocl = 0; vocl < MAX; vocl++){
-		length[vocl] = 0;
-	}
-	vocl = 0; 
-	while((c = getchar()) != EOF){
-		if(c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c == '-'){
-			vocl++;
-		} else{
-			length[vocl]++;
-			vocl = 0;
-		}
-	}
 
-	for(vocl = 0; vocl < MAX; vocl++){
-		if(length[vocl] != 0){
-			printf("%2d  ", vocl);
-			for(i = 1; i <= length[vocl]; i++){
-				putchar('*');
-			}
-			putchar('\n');
-		}
-	}
+    int i = 0;
+
+    int j = 0;
+
+    int c = 0;
+
+    int count = 0;
+
+    int overflow = 0;
+
+    int state = OUT;
+
+    int wlenth[MAXLEN];
+
+    for (i = 0; i < MAXLEN; i++)
+
+    {
+
+        wlenth[i] = 0;
+
+    }
+
+
+
+    while ((c = getchar()) != '\n') 
+
+    {
+
+        if (c == ' ' || c == '\t')
+
+        {
+
+            state = OUT;
+
+            if (count > 0)   
+
+            {
+
+                if (count <= MAXLEN)
+
+                    wlenth[count - 1]++;
+
+                else
+
+                    overflow++;
+
+                count = 0;
+
+            }
+
+        }
+
+        else if (state == OUT)  
+
+        {
+
+            state = IN;
+
+            count = 1;  
+
+        }
+
+        else
+
+            count++;  
+
+    }
+
+
+
+    printf("overflow - %2d: ", overflow);   
+
+    if (overflow > MAXHIST)
+
+        printf("OVER THE MAXHIST");
+
+    else
+
+    {
+
+        for (i = 0; i < overflow; i++)
+
+        {
+
+            putchar('*');
+
+        }
+
+    }
+
+    putchar('\n');
+
+
+
+    for (i = 0; i < MAXLEN; i++) 
+
+    {
+
+        printf("lenth%3d - %2d: ", i + 1, wlenth[i]);
+
+        if (wlenth[i] >= MAXHIST)
+
+            printf("OVER THE MAXHIST");
+
+        else
+
+        {
+
+            for (j = 0; j < wlenth[i]; j++)
+
+                putchar('*');
+
+        }
+
+        putchar('\n');
+
+    }
+
 }
-	
