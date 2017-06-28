37f0bd7b951be5954e2188af8882a79103e3d6bd
Caiyingying1 <809874260@qq.com>
2017-06-28 18:03:48 +0300

    Update c 16.cpp

--- язык с/c 16.cpp
+++ язык с/c 16.cpp
@@ -1,109 +1,75 @@
-/* strncpy(s, t, n)*/
-
-char *strncpy(char *s, const char *t, unsigned int n)
-
-{
-
-    char    *ptr = 0;
-
-    unsigned int i;
-
-
-
-    ptr = s;
-
-    for (i = 0; i < n && (*s++ = *t++); ++i)
-
-        ;
-
-    if (i == n && *--t)
-
-        *s  = '\0';
-
-    return ptr;
-
-}
-
-
-
-/* strcat(s, t, n) */
-
-char *strncat(char *s, const char *t, unsigned int n)
-
-{
-
-    char *ptr = 0;
-
-    unsigned int i;
-
-
-
-    ptr = s;
-
-    while (*s)
-
-        ++s;
-
-    for (i = 0; i < n && (*s++ = *t++); ++i)
-
-        ;
-
-    if (i == n && *--t)
-
-        *s  = '\0';
-
-    return ptr;
-
-}
-
-
-
-/* strncmp(s1, s2, n) */
-
-int strncmp(const char *s1, const char *s2, unsigned int n)
-
-{
-
-    unsigned int i;
-
-
-
-    for (i = 0; i < n && (*s1++ == *s2++); ++i)
-
-        ;
-
-    if (i == n)
-
-        return 0;
-
-    else
-
-        return (*--s1 - *--s2);
-
-}
-
-
-
 #include <stdio.h>
 
-/* §±§в§а§д§Ц§г§д§Ъ§в */
+#include <string.h>
 
-int main()
+
+
+
+
+int main (void)
 
 {
 
-    char s[100];
+    int n=0;
 
-    char t[]    = "hello world and...";
+    
 
-    char r[]    = "hello";
 
-    printf("%s\n", strncpy(s, t, 11));
 
-    printf("%s\n", strncat(s, t, 11));
+   char a[100];
 
-    printf("%d\n", strncmp(s, r, 3));
+   char c[100];
 
-    return 0;
+   printf("Vvedi stroku k a:");
+
+   gets(a);
+
+
+
+
+
+   printf("Vvedi c:");
+
+   gets(c);
+
+
+
+   printf("Vvedi n:");
+
+   scanf("%i", &n);
+
+   if (strncmp (a,c,n)==0){
+
+      printf("%i pervyh elementov ravny", n);
+
+      }
+
+      else{
+
+      printf("%i pervyh elementov ne ravny\n", n);
+
+      }
+
+
+
+
+
+   strncpy (a,c,n);
+
+
+
+   printf("%s\n", a);
+
+
+
+
+
+   strncat(c,a,n);
+
+   printf("stroka c s skopirovannymi %i simvolami iz tekuschey stroki a:\n%s", n, c);
+
+   
+
+   return 0;
 
 }
