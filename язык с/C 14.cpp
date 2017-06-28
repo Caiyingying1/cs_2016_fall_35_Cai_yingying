3a5a44c45f769c307c7b046b412ab582be6b3319
Caiyingying1 <809874260@qq.com>
2017-06-28 17:37:06 +0300

    Update C 14.cpp

--- язык с/C 14.cpp
+++ язык с/C 14.cpp
@@ -1,67 +1,35 @@
-#include<stdio.h>  
-       
-void Swap(char *a,char *b)  
-{  
-    char temp = *a;  
-    *a = *b;  
-    *b = temp;  
-}  
-       
+#include <stdio.h> 
 
-void Reverse(char *str,char *begin,char *end)  
-{  
-    if(str==NULL || begin==NULL || end==NULL)  
-        return;  
-       
-    while(begin < end)  
-    {  
-        Swap(begin,end);  
-        begin++;  
-        end--;  
-    }  
-}  
-       
+main() 
 
-void ReverseSentence(char *str)  
-{  
-    if(str == NULL)  
-        return;  
-       
-    char *begin = str;  
-    char *end = str;  
-    while(*end != '\0')  
-        end++;  
-    end--;  
-      
-    Reverse(str,begin,end);  
-       
-    
-    begin = str;  
-    while(*begin == ' ')  
-        begin++;  
-    end = begin;  
-    while(*begin != '\0')  
-    {  
-        while(*end!=' ' && *end!='\0')  
-            end++;  
-        end--;  
-        Reverse(str,begin,end);  
-       
-         
-        end++;  
-        while(*end == ' ')  
-            end++;  
-        begin = end;  
-    }  
-}  
-       
-int main()  
-{  
-    char str[50000];  
-    while(gets(str) != NULL)  
-    {  
-        ReverseSentence(str);  
-        puts(str);  
-    }  
-    return 0;  
-}
+{ 
+
+int n=0,i,j; 
+
+char a[10]; 
+
+printf("write sentence£º\n"); 
+
+for(i=0;a[i-1]!='\n';i++) 
+
+{ 
+
+scanf("%c",&a[i]); 
+
+n++; 
+
+} 
+
+printf("input£º\n"); 
+
+for(i=n-2;i!=-1;i--) 
+
+{ 
+
+printf("%c",*(a+i)); 
+
+} 
+
+putchar('\n'); 
+
+} 
