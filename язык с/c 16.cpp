5bd10804a7a5d172ace0bef4a2c3cfec5507bf45
Caiyingying1 <809874260@qq.com>
2017-06-28 17:38:15 +0300

    Update c 16.cpp

--- язык с/c 16.cpp
+++ язык с/c 16.cpp
@@ -1,19 +1,109 @@
-#include <stdio.h>
-void strncpy2(char *s,char *t,int n);
+/* strncpy(s, t, n)*/
 
-main()
+char *strncpy(char *s, const char *t, unsigned int n)
+
 {
-    char s[10],t[]="languagec ";
-    int n=5;
-    strncpy2(s,t,n);
-    printf("%s",s);
+
+    char    *ptr = 0;
+
+    unsigned int i;
+
+
+
+    ptr = s;
+
+    for (i = 0; i < n && (*s++ = *t++); ++i)
+
+        ;
+
+    if (i == n && *--t)
+
+        *s  = '\0';
+
+    return ptr;
+
 }
 
-void strncpy2(char *s,char *t,int n)
+
+
+/* strcat(s, t, n) */
+
+char *strncat(char *s, const char *t, unsigned int n)
+
 {
-    char *tempt;
-    tempt=t;
-    while(*t!='\0'&& t<tempt+n)
-        *s++=*t++;
-    *s='\0';
+
+    char *ptr = 0;
+
+    unsigned int i;
+
+
+
+    ptr = s;
+
+    while (*s)
+
+        ++s;
+
+    for (i = 0; i < n && (*s++ = *t++); ++i)
+
+        ;
+
+    if (i == n && *--t)
+
+        *s  = '\0';
+
+    return ptr;
+
+}
+
+
+
+/* strncmp(s1, s2, n) */
+
+int strncmp(const char *s1, const char *s2, unsigned int n)
+
+{
+
+    unsigned int i;
+
+
+
+    for (i = 0; i < n && (*s1++ == *s2++); ++i)
+
+        ;
+
+    if (i == n)
+
+        return 0;
+
+    else
+
+        return (*--s1 - *--s2);
+
+}
+
+
+
+#include <stdio.h>
+
+/* §±§в§а§д§Ц§г§д§Ъ§в */
+
+int main()
+
+{
+
+    char s[100];
+
+    char t[]    = "hello world and...";
+
+    char r[]    = "hello";
+
+    printf("%s\n", strncpy(s, t, 11));
+
+    printf("%s\n", strncat(s, t, 11));
+
+    printf("%d\n", strncmp(s, r, 3));
+
+    return 0;
+
 }
