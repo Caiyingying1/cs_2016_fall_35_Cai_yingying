c869ddcdd8d0c2c14336dacd9ee9c6cc464a91df
Caiyingying1 <809874260@qq.com>
2017-06-28 18:07:07 +0300

    Update c 10.cpp

--- язык с/c 10.cpp
+++ язык с/c 10.cpp
@@ -1,50 +1,41 @@
-
 #include <stdio.h>
 
-#include <stdlib.h>
 
-#include <stdio.h>
 
-unsigned rightrot(unsigned x,int n)
+
+
+unsigned int rightrot(unsigned int x,unsigned int n)
 
 {
 
-int i;
-
-for (i=0;i<n;i++) {
-
-if ((x & 1)==1)
-
-x= (x >>1) | ~(~0>>1); 
-
-else
-
-x=x>>1;
-
-}
-
-return x;
+    return (x>>n);
 
 }
 
 
 
-
-
-main ()
+int main()
 
 {
 
-unsigned rightrot(unsigned ,int);
 
-unsigned x;
 
-int n;
+	unsigned int x, n;
 
-scanf("%u %d",&x,&n);
+	printf("input x: ");
 
-printf("(%u,%d)=%u",x,n,rightrot(x,n));
+	scanf("%i",&x);
 
-system("pause");
+	printf("input n:");
+
+    scanf("%u", &n);
+
+	x = rightrot(x, n);
+
+	printf("%u", x);
+
+
+
+	return 0;
 
 }
