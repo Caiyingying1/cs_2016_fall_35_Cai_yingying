7963b8bff85814e31450138d8acec5beb5eff1e3
Caiyingying1 <809874260@qq.com>
2017-06-28 17:35:21 +0300

    Update c 20.cpp

--- язык с/c 20.cpp
+++ язык с/c 20.cpp
@@ -1,35 +1,49 @@
-# include <stdio.h>  
-# include <unistd.h>  
-# define NORMAL_SIZ BUFSIZ  
-   
-char buf[NORMAL_SIZ];  
-   
-#define CHMSK_KEY 0xa5  
-   
-int main(int argc, char **argv)  
-{  
-    FILE *fp = fopen(argv[1], "rb+");  
-    int n;  
-       
-    while ((n = fread(buf, 1, NORMAL_SIZ, fp)) > 0)  
-    {  
-        int i;  
-          
-        for (i = 0; i < n; i++)  
-        {  
-            buf[i] ^= CHMSK_KEY;  
-        }  
-           
-        if (fseek(fp, -n, SEEK_CUR) == -1)  
-        {  
-            perror("fseek");  
-        }  
-           
-        n = fwrite(buf, 1, n, fp);  
-        fflush(fp);
-        buf[0] = 0;  
-    }  
-   
-    fclose(fp);  
-    return 0;  
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <stdio.h>
+
+unsigned rightrot(unsigned x,int n)
+
+{
+
+int i;
+
+for (i=0;i<n;i++) {
+
+if ((x & 1)==1)
+
+x= (x >>1) | ~(~0>>1); 
+
+else
+
+x=x>>1;
+
+}
+
+return x;
+
+}
+
+
+
+
+
+main ()
+
+{
+
+unsigned rightrot(unsigned ,int);
+
+unsigned x;
+
+int n;
+
+scanf("%u %d",&x,&n);
+
+printf("(%u,%d)=%u",x,n,rightrot(x,n));
+
+system("pause");
+
 }
